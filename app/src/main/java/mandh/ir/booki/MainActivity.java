7dac43ba193c45a6d4e135c98871289ee8f240dc
mashtyyy <mashtyyy@gmail.com>
2019-06-10 18:16:32 +0430

    hosein test 0

--- app/src/main/java/mandh/ir/booki/MainActivity.java
+++ app/src/main/java/mandh/ir/booki/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello  ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello mjm  ", Toast.LENGTH_SHORT).show();
     }
 }
